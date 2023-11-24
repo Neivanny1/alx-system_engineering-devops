29ec1e6ca59c2fb1f1bb55baa085e1ac76950dbf
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-11-24 21:31:57 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,4 @@
-# Install Flask version 2.1.0 using pip3
-package { 'flask':
+# Installing flask
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
-}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
+  provider => 'pip3',}
