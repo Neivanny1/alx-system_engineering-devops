754e3e9d134c38c44f4014d652ee5ac0cfe73059
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-11-28 19:09:49 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,35 @@
-#!/usr/bin/env bash
+class { 'nginx':
+  ensure  => 'installed',
+  service => 'running',
+}
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => '
+    server {
+      listen 80;
+
+      location / {
+        return 200 "Hello World!\n";
+      }
+
+      location /redirect_me {
+        return 301 http://$host/;
+      }
+    }
+  ',
+  notify  => Service['nginx'],
+}
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => 'link',
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+  notify  => Service['nginx'],
+}
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'absent',
+}
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
+}
